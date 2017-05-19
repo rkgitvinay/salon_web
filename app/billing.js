f0eefc3024964c4611a862473d7ff530a77b8111
vinay singh <vinayksingh2@gmail.com>
2017-05-19 18:32:30 +0530

    Update at 19 may

--- app/billing.js
+++ app/billing.js
@@ -27,14 +27,73 @@ if(localStorage.getItem("access_token") == null){
 }else{
     var access_token = localStorage.getItem("access_token");  
 }
- var base_url = 'zalonstyle.in:8080';
-// var base_url = 'localhost:3000';
+// var base_url = 'zalonstyle.in:8080';
+var base_url = 'localhost:3000';
 
 phpro.controller('mainCtrl', function($scope,$http,$window) {
     $scope.invoice = 0;
     $scope.invoice_new = 0;
+    $scope.custom_price = 0;
+    var selectedItem = {};
+    var billing_events = [];
 
+
+
+    var customer_id = location.search.split('customer_id=')[1];
+    if(customer_id != undefined){
+        billing = {};
+        billing['access_token'] = access_token;
+        billing['customer_id']  = customer_id;
+        var data =  JSON.stringify(billing);
+        $http({
+            method  : 'POST',
+            url     : 'http://'+base_url+'/calendar/getEventServices',
+            data  : {payload:data}
+        }).then(function(response){
+            $scope.name = response.data.customer[0].name;
+            $scope.mobile = response.data.customer[0].mobile;
+            $scope.gender = response.data.customer[0].gender;
+            $scope.staffList = response.data.stylist;
+
+            if(response.data.package.length >0){
+                $scope.package_tab = true;
+                $scope.packageList = response.data.package;
+            }
+            if(response.data.membership.length > 0){
+                $scope.membership_tab = true;
+                $scope.membershipList = response.data.membership;    
+            }
+
+            if(response.data.service.length == 1){
+                selectedItem.id = response.data.service[0].service_id;
+                selectedItem.type = 'Service';
+                selectedItem.name = response.data.service[0].service;
+                $scope.service = selectedItem.name;
+                $scope.stylist = response.data.service[0].staff_id;
+                var staff_id = response.data.service[0].staff_id;
+                billing_events.push(response.data.service[0].event_id);
+                $scope.getPrice(staff_id);
+
+            }else if(response.data.service.length > 1){
+                $scope.apnt_cust = $scope.name+"'s appointments";  
+                $scope.appointmentList = true;
+                $scope.serviceList = response.data.service;
+            }
+        });
+    }
     
+    $scope.addServiceToBill = function(row){
+        //console.log(row);
+        selectedItem.id = row.service_id;
+        selectedItem.type = 'Service';
+        selectedItem.name = row.service;
+        $scope.service = selectedItem.name;
+        $scope.stylist = row.staff_id;
+        billing_events.push(row.event_id);
+        $scope.getPrice($scope.stylist);
+    }
+    
+
     var get_bill_info = function(){
         bill_info = {};
         bill_info['access_token']= access_token;
@@ -70,10 +129,25 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
         });
     }
 
+    var get_hold_list = {};
+    var getHoldCustomers = function(){
+        get_hold_list['access_token']    = access_token;
+        get_hold_list['mobile']          = 0;
+        var data =  JSON.stringify(get_hold_list);
+        $http({
+            method  : 'POST',
+            url     : 'http://'+base_url+'/billing/getHoldCustomers',
+            data  : {payload:data}
+        }).then(function(response){
+            console.log(response);
+            $scope.holdList = response.data.list;
+        });
+    }
+
     get_bill_info();
     getAllOtherItem();
-
-    var selectedItem;
+    getHoldCustomers();
+    
     $scope.packageInfoBox = false;
     $scope.price = 0;
     var search_arr = {};
@@ -155,6 +229,7 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
 
     var cust_arr = {};
     $scope.gender = 'Gender';
+
     $scope.getCustomerDetails = function(customer){
         $scope.name = customer.name;
         $scope.mobile = customer.mobile;
@@ -191,6 +266,7 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
     var package_info;
     var is_member = false;
     var is_package = false;
+
     $scope.getPrice = function(stylist){
         getPrice['access_token'] = access_token;
         getPrice['id']  = selectedItem.id;
@@ -198,7 +274,6 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
         getPrice['stylist']  = stylist;
         getPrice['customer_mobile']  = $scope.mobile;
         getPrice['gender']  = $scope.gender;
-
         var data =  JSON.stringify(getPrice);
         $http({
             method  : 'POST',
@@ -206,13 +281,13 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
             data  : {payload:data}
         }).then(function(response){
             if(response.data.status == 'success'){
-                console.log(response.data);
                 $scope.price = response.data.list.rate;
-
                 if(response.data.is_member == 'true'){
                     is_member = 'true';
                     $scope.discount = parseInt(response.data.member.discount.replace('%',''));
                     membership_info = response.data.member;
+                }else{
+                    is_member = 'false';
                 }
                 if(response.data.is_package == 'true'){
                     $scope.packageInfoBox = true;
@@ -220,6 +295,7 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
                     is_package = 'true';
                     package_info = response.data.package;
                 }else{
+                    is_package = 'false';
                     $scope.packageInfoBox = false;
                 }
             }
@@ -230,8 +306,6 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
     var item_list = {};
     var index = 1;
     $scope.calculateBill = function(){
-
-        // console.log($scope.discount);
 
         if($scope.discount == 0){
             var disc = 'No Discount';
@@ -250,12 +324,18 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
         }else{
             type = 'Product';
         }
+
+        if($scope.custom_price == 0){
+            var custom = 0;
+        }else{
+            var custom = 1;
+        }
         item_list['access_token']   = access_token;
         item_list['stylist']        = $scope.stylist;
         item_list['discounts']      = disc;
         item_list['gender']         = $scope.gender;
-        item_list['custom']         = 0;
-        item_list['custom_price']   = "",
+        item_list['custom']         = custom;
+        item_list['custom_price']   = $scope.custom_price,
         item_list['category']       = type;
         item_list['is_package']     = is_package.toString();   
         item_list['is_member']      = is_member.toString();
@@ -287,7 +367,7 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
                     response.data.list.discounts = 'No Discount';
                 }
                 // else{
-                //     var disc = response.data.list.discounts.toString();
+                //     var disc = response.data.list.discounts. toString();
                 //     response.data.list.discounts = disc+'%';
                 // }
 
@@ -298,9 +378,10 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
                 index++;
                 $scope.packageInfoBox = false;
                 $scope.price = 0;
+                $scope.custom_price = 0;
                 $scope.service = '';
-                $scope.staffList = [{id:0,category:'select staff'}];
-                $scope.stylist = 0;
+                //$scope.staffList = [{id:0,category:'select staff'}];
+                //$scope.stylist = 0;
                 $scope.quantity = 1;
                 $scope.discount = 0;
             }
@@ -341,20 +422,55 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
             url     : 'http://'+base_url+'/billing/generate_bill',
             data  : {payload:data}
         }).then(function(response){
+
                 $scope.payMethod = response.data.payment_methods;
-                $scope.payMethod.unshift({id:00,payment_method:'Select Pay Method'});
-                $scope.pay_id = 00;
+                $scope.payMethod.unshift({id:999999,payment_method:'Select Pay Method'});
+                $scope.pay_id = 999999;
                 $scope.sum = response.data.services[0];
                 $scope.total = response.data.total;
 
-                // $scope.packageInfoBox = false;
-                // $scope.price = 0;
-                // $scope.service = '';
-                // $scope.staffList = [{id:0,category:'select staff'}];
-                // $scope.stylist = 0;
-                // $scope.quantity = 1;
-                // $scope.discount = 0;
-            
+                if(response.data.prepaid_card == true){
+                    $scope.prepaidCardList = response.data.prepaid_cards;
+                    $scope.prepaidCardList.unshift({card_id:999999,card_name:'Select Card'});
+                    $scope.prepaid_card_id = 999999;
+                }
+        });
+    }
+
+    var hold_list = {};
+    $scope.holdBill = function(){
+        hold_list['access_token']    = access_token;
+        hold_list['customer_name']   = $scope.name;
+        hold_list['mobile']          = $scope.mobile;
+        hold_list['gender']          = $scope.gender;
+        hold_list['orders']            = $scope.info;
+
+        var data =  JSON.stringify(hold_list);
+        $http({
+            method  : 'POST',
+            url     : 'http://'+base_url+'/billing/addOrderToHold',
+            data  : {payload:data}
+        }).then(function(response){
+            if(response.data.status == 'success'){
+                $scope.info= [];
+                $scope.name = '';
+                $scope.mobile = '';
+                $scope.gender = '';
+                $scope.sum = [];
+                $scope.total = 0;
+                $scope.packageInfoBox = false;
+                $scope.membership_tab = false;
+                $scope.package_tab = true;
+                $scope.price = 0;
+                $scope.service = '';
+                $scope.staffList = [{id:0,category:'select staff'}];
+                $scope.stylist = 0;
+                $scope.quantity = 1;
+                $scope.discount = 0;
+                $scope.custom_price = 0;
+
+                getHoldCustomers();
+            }
         });
     }
 
@@ -371,6 +487,7 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
         chek_out['mobile']          = $scope.mobile;
         chek_out['payment_method']  = $scope.pay_id;
         chek_out['narration']       = '';
+        chek_out['event_id']        = billing_events;
 
         var data =  JSON.stringify(chek_out);
         $http({
@@ -380,26 +497,30 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
         }).then(function(response){
             if(response.data.status == 'success'){
                 $scope.info= [];
-
                 $scope.name = '';
                 $scope.mobile = '';
                 $scope.gender = '';
                 $scope.sum = [];
                 $scope.total = 0;
                 $scope.packageInfoBox = false;
+                $scope.appointmentList = false;
                 $scope.price = 0;
                 $scope.service = '';
                 $scope.staffList = [{id:0,category:'select staff'}];
                 $scope.stylist = 0;
                 $scope.quantity = 1;
                 $scope.discount = 0;
+                $scope.custom_price = 0;
                 get_bill_info();
+                getHoldCustomers();
             } 
         });
     }
+    
 
     var cancel = {};
     $scope.cancelOrder = function(){
+
         cancel['access_token']  = access_token;
         cancel['invoice_new']   = $scope.invoice_new;
         cancel['invoice']       = $scope.invoice;
@@ -407,36 +528,108 @@ phpro.controller('mainCtrl', function($scope,$http,$window) {
         cancel['array_new']     = [];
         cancel['type']          = 'service';
         cancel['mobile']        = $scope.mobile;
-
         var data =  JSON.stringify(cancel);
-        $http({
-            method  : 'POST',
-            url     : 'http://'+base_url+'/billing/cancelOrder',
-            data  : {payload:data}
-        }).then(function(response){
-            if(response.data.status == 'success'){
-                
-                $scope.info= [];
 
-                $scope.name = '';
-                $scope.mobile = '';
-                $scope.gender = '';
-                $scope.sum = [];
-                $scope.total = 0;
-                $scope.packageInfoBox = false;
-                $scope.price = 0;
-                $scope.service = '';
-                $scope.staffList = [{id:0,category:'select staff'}];
-                $scope.stylist = 0;
-                $scope.quantity = 1;
-                $scope.discount = 0;
-                get_bill_info();
-            } 
+        swal({
+          title: "Are you sure?",
+          //text: "You will not be able to recover this imaginary file!",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#DD6B55",
+          confirmButtonText: "Yes, Cancel it!",
+          closeOnConfirm: false
+        },
+        function(){
+
+            $http({
+                method  : 'POST',
+                url     : 'http://'+base_url+'/billing/cancelOrder',
+                data  : {payload:data}
+            }).then(function(response){
+                if(response.data.status == 'success'){
+                    $scope.info= [];
+                    $scope.name = '';
+                    $scope.mobile = '';
+                    $scope.gender = '';
+                    $scope.sum = [];
+                    $scope.total = '';
+                    $scope.packageInfoBox = false;
+                    $scope.membership_tab = false;
+                    $scope.prepaidCards = false;
+                    $scope.package_tab = true;
+                    $scope.price = 0;
+                    $scope.service = '';
+                    $scope.staffList = [{id:0,category:'select staff'}];
+                    $scope.stylist = 0;
+                    $scope.quantity = 1;
+                    $scope.discount = 0;
+                    $scope.payMethod = [{id:0,payment_method:'Select Pay Method'}];
+                    $scope.pay_id = 0;
+                    swal("Canceled!", "Your bill has been canceled.", "success");
+                    get_bill_info();
+                    getHoldCustomers();
+                } 
+            });  
         });
     }
 
-    $scope.test = function(test){
-        console.log(test);
+    $scope.selectGender = function(gen){
+        $scope.gender = gen;
     }
+
+    var index = 1;
+    $scope.getHoldBillByCustomer = function(mobile){
+        $scope.info = [];
+        var get_list = {};
+        get_list['access_token']    = access_token;
+        get_list['mobile']          = mobile;
+        var data =  JSON.stringify(get_list);
+        $http({
+            method  : 'POST',
+            url     : 'http://'+base_url+'/billing/getHoldCustomers',
+            data  : {payload:data}
+        }).then(function(response){
+            $scope.name = response.data.info.customer_name;
+            $scope.mobile= response.data.info.customer_mobile;
+            $scope.gender= response.data.info.gender;
+
+            response.data.list.forEach(function(list){
+                list.index = index;
+                list.is_stylist = list.stylist;
+                list.stylist    = list.staff_id;
+
+                if(list.discounts == 0){  
+                    list.discounts = 'No Discount';
+                }else{
+                    var disc = list.discounts.toString();
+                    list.discounts = disc+'%';
+                }
+                $scope.info.push(list);
+                index++;
+            });
+
+        });
+    }
+
+    function search(nameKey, myArray){
+        for (var i=0; i < myArray.length; i++) {
+            if (myArray[i].id === nameKey) {
+                return myArray[i];
+            }
+        }
+    }
+
+    $scope.testChange = function(val){
+        var result = search(val, $scope.payMethod);
+        if(result.payment_method == 'Prepaid Card'){
+            $scope.prepaidCards = true;
+        }else{
+            $scope.prepaidCards = false;
+        }
+    }
+
+
+
+
 
 });
